8c2663b98391ebfeae93676d22097897c08a0261
Rafael Mejia <devrafx@gmail.com>
2026-02-04 13:35:23 -0400

    Initial Commit

---
+++ PluralMac/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PluralMac
+//
+//  Created by Rafael Alexander Mejia Blanco on 4/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PluralMac/PluralMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  PluralMacApp.swift
+//  PluralMac
+//
+//  Created by Rafael Alexander Mejia Blanco on 4/2/26.
+//
+
+import SwiftUI
+
+@main
+struct PluralMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
